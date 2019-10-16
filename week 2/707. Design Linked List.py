80166b5ebacb24f8a29cd777b818e8133f3902a8
ykz0608 <55829277+ykz0608@users.noreply.github.com>
2019-10-16 23:24:23 +0800

    Create 707. Design Linked List.py

---
+++ week 2/707. Design Linked List.py
@@ -0,0 +1,53 @@
+class MyLinkedList(object):
+
+    def __init__(self):
+        """
+        Initialize your data structure here.
+        """
+        self.linkedlist = list()
+
+    def get(self, index):
+        """
+        Get the value of the index-th node in the linked list. If the index is invalid, return -1.
+        :type index: int
+        :rtype: int
+        """
+        if index < 0 or index >= len(self.linkedlist):
+            return -1
+        else:
+            return self.linkedlist[index]
+
+    def addAtHead(self, val):
+        """
+        Add a node of value val before the first element of the linked list. After the insertion, the new node will be the first node of the linked list.
+        :type val: int
+        :rtype: void
+        """
+        self.linkedlist.insert(0, val)
+
+    def addAtTail(self, val):
+        """
+        Append a node of value val to the last element of the linked list.
+        :type val: int
+        :rtype: void
+        """
+        self.linkedlist.append(val)
+
+    def addAtIndex(self, index, val):
+        """
+        Add a node of value val before the index-th node in the linked list. If index equals to the length of linked list, the node will be appended to the end of linked list. If index is greater than the length, the node will not be inserted.
+        :type index: int
+        :type val: int
+        :rtype: void
+        """
+        if 0 <= index and index <= len(self.linkedlist):
+            self.linkedlist.insert(index, val)
+
+    def deleteAtIndex(self, index):
+        """
+        Delete the index-th node in the linked list, if the index is valid.
+        :type index: int
+        :rtype: void
+        """
+        if 0 <= index and index < len(self.linkedlist):
+            self.linkedlist.pop(index)
