4b42a0b543f3f55f47613d4a0f55392cd77910bd
ykz0608 <55829277+ykz0608@users.noreply.github.com>
2019-11-27 08:33:26 +0800

    Update binary_search_tree_06170271.py

--- HW3/binary_search_tree_06170271.py
+++ HW3/binary_search_tree_06170271.py
@@ -15,54 +15,60 @@ class Solution(object):
             if root.val == None:
                 root.left= TreeNode(val)
             else:
-                root.left = self.insert(root.left, val)
+                self.insert(root.left, val)
+        elif val >root.val:
+            if root.val == None:
+                root.right= TreeNode(val)
+            else:
+                self.insert(root.right, val)
+        else:
+            return None
+            
+    def insert_pre(self, root, val):
+        if root == None:
+            root = TreeNode(val)
+        elif val <= root.val:
+            if root.val == None:
+                root.left= TreeNode(val)
+            else:
+                root.left = self.insert_pre(root.left, val)
         elif val > root.val:
             if root.val == None:
                 root.right= TreeNode(val)
             else:
-                root.right = self.insert(root.right, val)
+                root.right = self.insert_pre(root.right, val)
         return root
+
+    def preorder(self, root):
+        if root == None:
+            return []
+        return [root.val] + self.preorder(root.left) + self.preorder(root.right)
     
-   
-    def preorder(self,root):
-        if root is None:return []
-        result=[root.val]
-        resultl=self.preorder(root.left)
-        resultr=self.preorder(root.right)
-        return result+resultl+resultr
-        
     def search(self, root, target):
         if root == None:
             return False
-        if root.val == target:
-            return root
-        elif target <= root.val:
-            return self.search(root.left,target)
-        elif target > root.val:
-            return self.search(root.right,target)
+        else:
+            if root.val == target:
+                return root
+            elif target <= root.val:
+                return self.search(root.left,target)
+            elif target > root.val:
+                return self.search(root.right,target)
+    
         
-    def findMin(self, root):
+    def FM(self, root):
         if root.left:
-            return self.findMin(root.left)
+            return self.FM(root.left)
         else:
             return root
-        
-    def findMax(self, root):
-        if root.right:
-            return self.findMax(root.right)
-        else:
-            return root
-        
+    
     def delete(self, root, target):
         if root == None:
-            return 
-        if target < root.val:
-            root.left = self.delete(root.left, target)
-        elif target > root.val:
-            root.right = self.delete(root.right, target)
-        else:
+            return None
+        
+        elif target == root.val:
             if root.left and root.right:
-                temp = self.findMin(root.right)
+                temp = self.FM(root.right)
                 root.val = temp.val
                 root.right = self.delete(root.right, temp.val)
             elif root.right == None and root.left == None:
@@ -71,11 +77,20 @@ class Solution(object):
                 root = root.left
             elif root.left == None:
                 root = root.right
+            else:
+                return None
+        
+        elif root.val> target:
+            root.left = self.delete(root.left, target)
+            
+        elif root.val< target:
+            root.right = self.delete(root.right, target)
+            
         return root
     
     def modify(self, root, target, new_val):
         if root == None:
-            return False
+            return 
         else:
             if root.val == target:
                 root.val = new_val
@@ -87,3 +102,4 @@ class Solution(object):
                 self.modify(root.right, target, new_val)
             
         return root
+    
