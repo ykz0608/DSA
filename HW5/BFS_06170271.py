879413ddfe6f390bbb42d0c3f87d60a33fae0565
ykz0608 <55829277+ykz0608@users.noreply.github.com>
2019-12-20 22:19:26 +0800

    Update BFS_06170271.py

--- HW5/BFS_06170271.py
+++ HW5/BFS_06170271.py
@@ -1,1 +1,42 @@
-s
+from collections import defaultdict 
+
+class Graph:
+    def __init__(self): 
+        self.graph = defaultdict(list) 
+
+    def addEdge(self,u,v): 
+        self.graph[u].append(v) 
+  
+    def BFS(self, s): 
+        queue = [] 
+        output = []
+        if len(output)==0:
+            output.append(s)
+            queue.extend(self.graph[s])  
+        
+        while len(queue) != 0 :   
+            vertex = queue[0]
+            output.append(vertex)
+            queue.pop(0)
+            for i in self.graph[vertex]:
+                if i not in queue and i not in output:
+                    queue.append(i)
+        return output
+        
+    def DFS(self, s): 
+        queue = [] 
+        output = []
+        if len(output)==0:
+            output.append(s)
+            queue.extend(self.graph[s])  
+        
+        while len(queue) != 0 :   
+            vertex = queue[-1]
+            output.append(vertex)
+            queue.pop(-1)
+            for i in self.graph[vertex]:
+                if i not in queue and i not in output:
+                    queue.append(i)
+        return output
+      
+
